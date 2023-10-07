3766252769c545a05773400c1d5ea1cbce3335d2
srushtigadale <srushtirg22.comp@coep.ac.in>
2023-10-07 11:14:16 +0530

    second commit

--- q2.c
+++ q2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Enter 3 numbers");
+    printf("Enter 3 numbers , Srushti");
     int a,b,c;
     scanf("%d%d%d" , &a , &b , &c);
     (a>b && a>c)?printf("%d is the largest" , a):(b>a && b>c)?printf("%d is the largest" , b):printf("%d is the largest12" , c);
